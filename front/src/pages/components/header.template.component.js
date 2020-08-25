ec431e41a3c753d1fb822d9534796d8ef34c9135
songjonggeun <cops121@gmail.com>
2020-08-26 03:07:10 +0900

    메인수정

--- front/src/pages/components/header.template.component.js
+++ front/src/pages/components/header.template.component.js
@@ -1,22 +1,22 @@
-template = `<div class="navbar no-hairlines no-hairlines">
-            <div class="navbar-bg"></div>
-            <div class="navbar-inner">
-                <div class="row align-items-center width-100 color-theme-teal">
-                    <div class="title">{{$props.title}}</div>
-                    <div class="row">
-                    {{#unless $props.nomenu}}
-                        <button class="button col panel-open width-auto" data-panel=".panel-left">
-                            <i class="f7-icons">bars</i>
-                        </button>
-
-                        <button class="button col panel-open width-auto" data-panel=".panel-right">
-                            <i class="f7-icons">person_crop_circle_fill</i>
-                        </button>
-                    {{/unless}}
+template = `<div class="navbar navbar-transparent">
+                <div class="navbar-bg"></div>
+                <div class="navbar-inner">
+                    <div class="row align-items-center width-100">
+                        {{#unless $props.nomenu}}
+                            <div class="left">
+                                <button class="button col panel-open width-auto" data-panel=".panel-left">
+                                <i class="f7-icons">bars</i>
+                                </button>
+                            </div>
+                            <div class="title">{{$props.title}}</div>
+                            <div class="right">
+                                <button class="button col panel-open width-auto" data-panel=".panel-right">
+                                <i class="f7-icons">person_crop_circle_fill</i>
+                                </button>
+                            </div>                                
+                        {{/unless}}                    
                     </div>
                 </div>
-
-            </div>
         </div>`
 
 Framework7.registerComponent('main-header', {
